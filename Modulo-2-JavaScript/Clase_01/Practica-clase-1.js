d629d015001d4e3d3404d0424522238a0ecbf93f
Carlos Solis <carlossolisschulz@gmail.com>
2023-07-20 18:16:26 -0300

    subo practicas de la clase 1 del modulo 2

---
+++ Modulo-2-JavaScript/Clase_01/Practica-clase-1.js
@@ -0,0 +1,31 @@
+/*
+practicas pdf clase 01 y javaScript, Mar del Plata Programa
+*/
+
+
+
+// imprimir nombre por consola
+
+console.log("carlos");
+
+//sumar dos numeros
+
+console.log(2 + 3);
+
+//concatenar nombre y edad
+
+let a = "carlos";
+
+let b = 500;
+
+console.log(a + ": " + b);
+
+//comparar dos numeros si es mayor o menor
+
+let c = 5;
+
+let d = 7;
+
+let res = c < d ? "d es mayor" : "c es mayor";
+
+console.log(res);
