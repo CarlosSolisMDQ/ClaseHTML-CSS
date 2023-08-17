05d714d6b135247946443945d4655dea570f012e
Carlos Solis <carlossolisschulz@gmail.com>
2023-08-17 19:02:45 -0300

    Creo que quedó terminado, por lo menos los puntos pedidos que recuerdo.

--- Modulo-2-JavaScript/Clase_08/index.js
+++ Modulo-2-JavaScript/Clase_08/index.js
@@ -9,56 +9,107 @@
  * 
  * 
  */
-let dataProduct = [];
+let dataProducts = [];
 
-const addDataProduct = (ID, description, precio) => {
+/**
+ * estos dos bloques los dejé así mientras estaba probando cosas.
+ * 
+ * listo los productos en un json, para que se pueda editar la lista de productos 
+ * en una sola operacion en el futuro. Luego por joder desde ese json extraigo
+ * los precios y los inserto en el documento. 
+ */
+
+
+let dataProduct = [{id: 0, descripcion: "producto1", precio: 100},
+{id: 1, descripcion: "producto2", precio: 150},
+{id: 2, descripcion: "producto3", precio: 50},
+{id: 3, descripcion: "producto4", precio: 90}];
+
+
+
+document.getElementById("precio01").innerHTML = dataProduct[0].precio;
+document.getElementById("precio02").innerHTML = dataProduct[1].precio;;
+document.getElementById("precio03").innerHTML = dataProduct[2].precio;;
+document.getElementById("precio04").innerHTML = dataProduct[3].precio;;
+
+const addDataProduct = (id, description, precio) => {
     let producto = {
-        ID,
+        id,
         description,
         precio
     }
     dataProducts.push(producto);
 }
  
+/**
+ * Funcion de agregado. Puse un id en el botón de la card del producto, al llamar a this.id baja la id 
+ * y identifico al producto. Lo mando al array y a la sessionStorage. Listo los productos desde la session
+ * storage
+ * calculo la suma.
+ */
 
-const clickAddProduct = () =>{
+const clickAddProduct = (id) =>{
 
-
-    
-
-    
-    
-    alert("funciona");
-    
-    
-    
-
-
-    
+    addDataProduct(dataProduct[id].id, dataProduct[id].descripcion, dataProduct[id].precio);
+    sessionStorage.arr = JSON.stringify(dataProducts);
+    let carrito = JSON.parse(sessionStorage.arr);
+    listDataProducts(carrito);
+    console.table(carrito);
+    sumatoriaTotal(carrito);
            
 }
+
+/**
+ * La funcion de listado de douglas pero con algunos cambios para simplificarla. 
+ * Cambié las referencias a los datos
+ */
 
 const listDataProducts = (arrayDataProducts) =>{
     let list = '';
     
-
-    arrayDataPersona.forEach( (product,index) => {
-        let status = returnStatuString(parseInt(product.status));
+    arrayDataProducts.forEach( (product,index) => {
+        
         list += `<tr>
-                <td>${ID}</td>
-                <td>${Product}</td>
-                <td>${precio}</td>
+                <td>${product.id}</td>
+                <td>${product.description}</td>
+                <td>${product.precio}</td>
                 
-                <td><button class="btn btn-primary" onclick="deleteProductOne(${ID})">Eliminar</button></td>
+                <td><button class="btn btn-primary" onclick="deleteProductOne(${product.id})">Eliminar</button></td>
                 </tr>`;
     });
 
 
-    document.getElementById("listProducs").innerHTML = list;
+    document.getElementById("listProducts").innerHTML = list;
 
 }
 
+/**
+ * funcion de calculo total. No tendría que hacerlo pero la funcion edita el DOM y ingresa el dato de la suma
+ */
 
+let sumatoriaTotal = (arrayDataProducts) => {
+    let acc = 0; 
+    dataProducts.forEach((item)=> acc = acc + item.precio);
+    console.log(acc)
+    document.getElementById("total").innerHTML = acc;
+    
+    
+}
+
+/**
+ * esto releva si el sessionStorage esta lleno, si es así lista el carrito y calcula el total.
+ */
+
+
+if(sessionStorage.getItem("arr") != null){
+    dataProducts = JSON.parse(sessionStorage.arr);
+    listDataProducts(dataProducts);
+    sumatoriaTotal(dataProducts);
+}
+
+
+
+/*
 const filterInput = () =>{
     let search = document.getElementById("search").value;
     search = search.trimStart().toLowerCase();
@@ -75,6 +126,7 @@ const dataFilterProduct = (search) =>{
                
     })
 }
+*/
 
 const deleteProductOne = (ID) =>{
     Swal.fire({
@@ -89,15 +141,22 @@ const deleteProductOne = (ID) =>{
         if (result.isConfirmed) {
 
             /**
-             * modifico el codigo de doglas haciendo que busque por dni la posicion en el array madre
-             * antes buscaba por posicion pero tomando como referencia el array generado en la busqueda
-             * ahora se ingresa por el html el dni del json y luego busco con eso la posicion para borrar.
+             * 
+             * usando el ejemplo anterior de douglas modifico para que la operacion de borrado sobre el array 
+             * tenga como agente intermedio al session storage
+             * 
+             * 
              */
 
 
-            let index = dataProduct.findIndex((product) => ID == product.ID);
-            dataPerson.splice(index,1);
-            listDataPerson(dataProduct);
+            let index = dataProducts.findIndex((product) => ID == product.ID);
+            dataProducts.splice(index,1);
+            sessionStorage.arr = JSON.stringify(dataProducts);
+            let carrito = JSON.parse(sessionStorage.arr);
+            listDataProducts(carrito);
+            console.table(carrito);
+            sumatoriaTotal(carrito);
+            
           Swal.fire(
             'Eliminado!',
             'El resgitro fue eliminado.',
